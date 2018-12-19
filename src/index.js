1e7d2e65ee9283a9ccbebcd03d0196c420492440
xiaomagexiao <1583884738@qq.com>
2018-12-19 11:45:58 +0800

    1111

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import React from "react";
+//    import React from "react";
 import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
